6acb46f6b6127ea99331421eb046cc13cc039552
kmadhaveswar <kmadhaveswar@vmware.com>
2018-10-14 15:00:32 +0530

    Third Commit

---
+++ src/main/java/tacos/ApplicationMetricsApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ApplicationMetricsApplication extends SpringBootServletInitializer  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMetricsApplication.class, args);
+	}
+}
