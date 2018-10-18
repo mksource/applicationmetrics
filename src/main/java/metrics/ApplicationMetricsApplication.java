f9698f50fdb8f8b59419c67b0f00d64b4ed62592
kmadhaveswar <kmadhaveswar@vmware.com>
2018-10-18 12:53:34 +0530

    Normal Commit 1

--- src/main/java/metrics/ApplicationMetricsApplication.java
+++ src/main/java/metrics/ApplicationMetricsApplication.java
@@ -3,8 +3,10 @@ package metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"metrics", "metrics.service,metrics.controller"})
 public class ApplicationMetricsApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
--- src/main/java/metrics/controller/ApplicationMericsCollector.java
+++ src/main/java/metrics/controller/ApplicationMericsCollector.java
@@ -1,24 +1,29 @@
 package metrics.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import metrics.service.MetricsCollectorService;
+
 @Controller
 public class ApplicationMericsCollector {
 
+	@Autowired
+	private MetricsCollectorService metricsCollectorService;
 	
 	@GetMapping
 	public ResponseEntity<HttpStatus> startCollectingMetrics() {
-		
+		metricsCollectorService.start();
 		return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
 	@GetMapping
 	public ResponseEntity<HttpStatus> stopCollectingMetrics() {
-		
+		metricsCollectorService.stop();
 		return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
