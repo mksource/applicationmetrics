00a05eb5fc1e9aaa95dc4fb026c43578266a87d2
kmadhaveswar <kmadhaveswar@vmware.com>
2018-10-22 21:17:26 +0530

    Deploying New Build

--- src/main/java/metrics/controller/HelloWorldController.java
+++ src/main/java/metrics/controller/HelloWorldController.java
@@ -17,7 +17,7 @@ public class HelloWorldController {
 
     @GetMapping("/")
     @ResponseBody
-    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Blue Environment") String name) {
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Green Environment") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
